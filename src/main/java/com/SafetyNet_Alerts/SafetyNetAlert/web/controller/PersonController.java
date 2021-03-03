7da29d0eecc190ab638c60168b527b37a3179c37
sleabykhalil <khalilsleaby@hotmail.com>
2021-03-03 17:27:36 +0100

    initialize controllers , add getAll

--- src/main/java/com/SafetyNet_Alerts/SafetyNetAlert/web/controller/PersonController.java
+++ src/main/java/com/SafetyNet_Alerts/SafetyNetAlert/web/controller/PersonController.java
@@ -1,4 +1,20 @@
 package com.SafetyNet_Alerts.SafetyNetAlert.web.controller;
 
+import com.SafetyNet_Alerts.SafetyNetAlert.dao.PersonDao;
+import com.SafetyNet_Alerts.SafetyNetAlert.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class PersonController {
+    @Autowired
+    PersonDao personDao;
+
+    @GetMapping(value = "/Persons")
+    public List<Person> getAllPersons(){
+        return personDao.findAll();
+    }
 }
