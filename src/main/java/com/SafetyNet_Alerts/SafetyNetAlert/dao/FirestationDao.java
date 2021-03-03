13992cde3629af0967ede1bfaf36a20fddf6acfe
sleabykhalil <khalilsleaby@hotmail.com>
2021-03-03 17:03:21 +0100

    Create Repositories

--- src/main/java/com/SafetyNet_Alerts/SafetyNetAlert/dao/FirestationDao.java
+++ src/main/java/com/SafetyNet_Alerts/SafetyNetAlert/dao/FirestationDao.java
@@ -1,4 +1,9 @@
 package com.SafetyNet_Alerts.SafetyNetAlert.dao;
 
-public interface FirestationDao {
+import com.SafetyNet_Alerts.SafetyNetAlert.model.Firestation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FirestationDao extends JpaRepository<Firestation,Integer> {
 }
--- src/main/java/com/SafetyNet_Alerts/SafetyNetAlert/dao/MedicalRecordDao.java
+++ src/main/java/com/SafetyNet_Alerts/SafetyNetAlert/dao/MedicalRecordDao.java
@@ -1,4 +1,9 @@
 package com.SafetyNet_Alerts.SafetyNetAlert.dao;
 
-public interface MedicalRecordDao {
+import com.SafetyNet_Alerts.SafetyNetAlert.model.MedicalRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicalRecordDao extends JpaRepository<MedicalRecord,Integer> {
 }
