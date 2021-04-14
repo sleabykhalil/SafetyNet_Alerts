6bfc7953a9b265092a954c0e91be32a4a5897c91
sleabykhalil <khalilsleaby@hotmail.com>
2021-04-14 16:26:10 +0200

    Tdd Create Test for MedicalRecordRepository

--- src/main/java/com/SafetyNet_Alerts/SafetyNetAlert/repository/MedicalRecordRepository.java
+++ src/main/java/com/SafetyNet_Alerts/SafetyNetAlert/repository/MedicalRecordRepository.java
@@ -2,29 +2,34 @@ package com.SafetyNet_Alerts.SafetyNetAlert.repository;
 
 import com.SafetyNet_Alerts.SafetyNetAlert.constants.JsonDataFileName;
 import com.SafetyNet_Alerts.SafetyNetAlert.model.MedicalRecord;
+import com.SafetyNet_Alerts.SafetyNetAlert.servec.Services;
 import com.SafetyNet_Alerts.SafetyNetAlert.tools.JsonFileRW;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-@AllArgsConstructor
 public class MedicalRecordRepository {
-
     public static List<MedicalRecord> medicalRecordList = new ArrayList<>();
 
+    @Autowired
+    private Services services;
+    @Autowired
+    private JsonFileRW jsonFileRW;
+
     /**
-     * Data initialize get all Medical records from json file
+     * Constructor and Data initialize get all Medical records from json file
+     *
+     * @param services
+     * @param jsonFileRW
      */
-    @PostConstruct
-    private void setup() {
-        JsonFileRW jsonFileRW = new JsonFileRW();
+    public MedicalRecordRepository(final Services services, final JsonFileRW jsonFileRW) {
+        this.services = services;
+        this.jsonFileRW = jsonFileRW;
         medicalRecordList = jsonFileRW.jsonAsStringToJsonFileModel(jsonFileRW.jsonFileToString(JsonDataFileName.dataFileName)).getMedicalrecords();
     }
-
 
     public List<MedicalRecord> findAll() {
         List<MedicalRecord> result;
